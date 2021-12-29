b1d3eef7253ea64333243ed3db4b941935c7b2f2
Andereh <ronaldherpc@gmail.com>
2021-12-29 00:40:02 -0800

    New visuals and funcionality added

--- ruffini.py
+++ ruffini.py
@@ -13,21 +13,24 @@ def calcularRes(coc, datos):
 	return _restos, _sustraendos
 
 
-base = [1, -9, 4, 12]
+base = []
 sustraendos = []
 restos = []
 
 nums = [-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6, -7, 7, -8, 8, -9, 9]
 cocientes = [' ']
+i = 1
 
-for num in nums:
-	restos, sustraendos = calcularRes(num, base)
-	if restos[-1] == 0:
-		cocientes[0] = num
-		break
-if cocientes[0] == ' ':
- 	cocientes[0] = 'X'
-
+print("\nIntroduce los valores (0 para salir)\n")
+while True:
+    temp = int(input(f"    Termino #{i}: "))
+    if temp != 0:
+        base.append(temp)
+    if temp == 0:
+        break
+    i = i+1
+print("\nResolucion del ejercicio")
+#Impresion de datos
 print()
 print('{:^5}|'.format(''), end='')
 
@@ -35,23 +38,41 @@ for i in range(0, len(base)):
 	print('{:^5}'.format(base[i]), end='')
 print()
 
-if cocientes[0] != ' ':
-	print(Fore.CYAN, end='')
-print('{:^5}'.format(cocientes[0]) + Fore.RESET + '|', end='')
-print('{:^5}'.format(''), end='')
+while len(base) > 1:
+    for num in nums:
+	    restos, sustraendos = calcularRes(num, base)
+	    if restos[-1] == 0:
+		    cocientes[0] = num
+		    break
+    if cocientes[0] == ' ':
+ 	    cocientes[0] = 'X'
 
-for i in range(0, len(sustraendos)):
-	print('{:^5}'.format(sustraendos[i]), end='')
 
-print()
-print('-----'*(len(restos) + 1))
-print('{:^5}|'.format(''), end='')
+    if cocientes[0] != ' ':
+	    print(Fore.CYAN, end='')
+    print('{:^5}'.format(cocientes[0]) + Fore.RESET + '|', end='')
+    print('{:^5}'.format(''), end='')
 
-for i in range(0, len(restos)):
+    for i in range(0, len(sustraendos)):
+	    print('{:^5}'.format(sustraendos[i]), end='')
 
-	if i == len(restos) - 1:
-		if restos[-1] == 0:
-			print(Fore.GREEN, end='')
-		else:
-			print(Fore.YELLOW, end='')
-	print('{:^5}'.format(restos[i]), end='')
+    print()
+    print('-----'*(len(restos) + 1))
+    print('{:^5}|'.format(''), end='')
+
+    for i in range(0, len(restos)):
+
+	    if i == len(restos) - 1:
+		    if restos[-1] == 0:
+			    print(Fore.GREEN, end='')
+		    else:
+			    print(Fore.YELLOW, end='')
+	    print('{:^5}'.format(restos[i]), end='')
+
+    if restos.pop() == 0:
+        base = restos
+    else:
+        print(Fore.RED, end='')
+        print("\nError: Imposible factorizar")
+        break
+    print()
