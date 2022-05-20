86d55e3db4dd45b82d57070c3c50df8e82a1c08f
Andereh <ronaldherpc@gmail.com>
2022-05-20 19:05:38 -0400

    All in a single file

--- cpp-files/factor.cpp
+++ cpp-files/factor.cpp
@@ -1,7 +1,32 @@
+#include <stdio.h>
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+
+bool is_prime(int number);
+void next_primo(int * _nprimo);
+string get_data(int* base);
+
+int nprimo = 2;
+unsigned t0, t1;
+
+int main()
+{
+    int base; 
+    string my_string;
+
+    cout << "num a facorizar: ";
+    scanf("%d", &base);
+
+    my_string = get_data(&base); // Toda la logica y lo "complicado" esta en mcm.h
+    cout << "\nFatores primos: "<< my_string << "\n";
+
+    fflush(stdin);
+    getchar();
+
+    return 0;
+}
 
 
 bool is_prime(int number)
