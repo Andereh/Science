0e5a660a8697de47c53d69f22b6d0c8502354181
Andereh <ronaldherpc@gmail.com>
2022-06-08 21:50:32 -0400

    New 2

--- files-java/Ecuaciones.java
+++ files-java/Ecuaciones.java
@@ -1,22 +1,19 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class Ecuaciones {
-
-    public static void main(String[] args)
+class Polinomio {
+    private double result1, result2, innerSqrt;  // InnerSqrt se refiere a la parte internar de la raiz
+                                                  // Lo hacemos asi para hacer el programa mas legible
+    private int a, b, c;
+    
+    public void setValues(int _a, int _b, int _c)
     {
-        Scanner scan = new Scanner(System.in);
-        double a, b, c, result1, result2, innerSqrt;  // InnerSqrt se refiere a la parte internar de la raiz
-                                                    // Lo hacemos haci para hacer el programa mas legible
+        a = _a;
+        b = _b;
+        c = _c;
+    }
 
-        System.out.print("Valor de a: ");
-        a = scan.nextFloat();
-
-        System.out.print("Valor de b: ");
-        b = scan.nextInt();
-
-        System.out.print("Valor de c: ");
-        c = scan.nextInt();
-
+    public void getResults()
+    {
         innerSqrt = Math.sqrt( Math.abs( Math.pow(b, 2) - 4*a*c) ); // utilizamos el valor absoluto de innerSqrt
                                                                     // para no trabajar con numeros imaginarios
 
@@ -25,6 +22,30 @@ public class Ecuaciones {
 
         System.out.println("\nResultado 1: " + result1);
         System.out.println("Resultado 2: " + result2);
+
+    }
+
+}
+
+public class Ecuaciones {
+
+    public static void main(String[] args)
+    {
+        int a, b, c;
+        Polinomio poli = new Polinomio();
+
+        a = Integer.parseInt(JOptionPane.showInputDialog(null, "Valor de a"));
+        b = Integer.parseInt(JOptionPane.showInputDialog(null, "Valor de b"));
+        c = Integer.parseInt(JOptionPane.showInputDialog(null, "Valor de c"));
+
+         /*
+          * No utilizamos excepciones ya que cualquier valor no real sera correcto
+          */
+
+
+        poli.setValues(a, b, c);
+        poli.getResults();
+
 
     }
 }
