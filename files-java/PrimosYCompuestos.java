de91fb27e9d6af9dd0ad75da208b26f63f3572e1
Andereh <ronaldherpc@gmail.com>
2022-06-08 22:23:53 -0400

    New 3

--- files-java/PrimosYCompuestos.java
+++ files-java/PrimosYCompuestos.java
@@ -1,8 +1,17 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class PrimosYCompuestos {
-	public static boolean is_prime(int n)
+class NumChecker {
+    private int n;
+
+    public void setValue(int _n)
+    {
+        n = _n;
+    }
+
+
+	private boolean is_prime()
 	{
+        if (n == 2) return true;
 		if (n%2 == 0) return false;
 
 		for (int i = 3; i < n/2; i += 2) {
@@ -10,20 +19,38 @@ public class PrimosYCompuestos {
 		}
 		return true;
 	}
-
-    public static void main(String[] args)
+    
+    public void check()
     {
-        Scanner scan = new Scanner(System.in);
-        int num; 
-        System.out.print("Introduce un numero: ");
-
-        num = scan.nextInt();
-        if (is_prime(num))
+        if (is_prime())
         {
         	System.out.println("Es un numero primo");
         } else
         {
         	System.out.println("Es un numero compuesto");
         }
+
+    }
+}
+
+public class PrimosYCompuestos {
+
+    public static void main(String[] args) throws Exception
+    {
+        int num; 
+
+        NumChecker checker = new NumChecker();
+
+        num = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el numero a verificar"));
+
+        if (num <= 1)
+        {
+            throw new Exception("El numero no puede ser inferior a 2");
+        }
+
+        checker.setValue(num);
+        checker.check();
+
+
     }
 }
