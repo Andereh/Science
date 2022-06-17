b624e0643046aaaf08274171740a2237f14454d7
Andereh <ronaldherpc@gmail.com>
2022-06-17 15:54:36 -0400

    Ejercicio 01

--- files-cpp/descomponer-ns.cpp
+++ files-cpp/descomponer-ns.cpp
@@ -10,6 +10,11 @@ int main() {
 	cout << "Que numero quieres descomponer: ";
 	cin >> n;
 
+	while (n < 1) {
+		cout << "El numero debe ser positivo, intenta otro: ";
+		cin >> n;
+	}
+
 	for (int i = 0; n > 0; i++) {
 		cout << n%10 * pow(10, i) << "\n";
 		n /= 10;
