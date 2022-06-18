0f034eebe78d4e8b29b64e7cde3dd56725426c5c
Andereh <ronaldherpc@gmail.com>
2022-06-17 23:21:24 -0400

    Troubleshoting

--- files-cpp/calc-cifras.cpp
+++ files-cpp/calc-cifras.cpp
@@ -9,6 +9,11 @@ int main() {
 	cout << "Programa que te dice cuantas cifras tiene un numero\n";
 	cout << "Ingrese un numero: ";
 	cin >> num;
+	
+	while (num < 1) {
+		cout << "El numero debe ser positivo, intenta otro: ";
+		cin >> num;
+	}
 
 	n_cifras = to_string(num).length();
 	
